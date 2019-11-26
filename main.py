b17becbbc99a68911cc4747c75d433b468dec93e
KoHanjun <rhgkswns@naver.com>
2019-11-26 11:06:46 +0900

    import Pyserial / pyqt5 / pyinstall

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import serial
+import time
+import threading
+import datetime
+
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,  QPushButton, QVBoxLayout, QHBoxLayout,QLabel,
+QPushButton, QTextEdit,  QGridLayout, QRadioButton, QSlider,  QMessageBox,QTabWidget, QProgressBar)
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import QtWidgets
+
 print('hellow World')
